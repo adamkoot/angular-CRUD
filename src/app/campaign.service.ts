5abb28af57b0d01196a8539081fcc9bea8892b22
Adam <adam.kot511@gmail.com>
2023-11-17 18:33:40 +0100

    push26

--- src/app/campaign.service.ts
+++ src/app/campaign.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpHeaders  } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: HttpClientModule
 })
+
+
+
 export class CampaignService {
   private baseUrl = '/api/campaigns';
 
   constructor(private http: HttpClient) {}
 
+    httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+      // Dodaj inne nagłówki, jeśli są potrzebne
+    })
+  };
+  
+
   getAllCampaigns(): Observable<any> {
-    this.http.get(this.baseUrl).subscribe(
+    this.http.get(this.baseUrl, this.httpOptions).subscribe(
         (data) => {
           console.log(data)
         },
@@ -19,7 +30,7 @@ export class CampaignService {
           console.error('Błąd w zapytaniu HTTP:', error);
         }
       );
-      return this.http.get(this.baseUrl);
+      return this.http.get(this.baseUrl, this.httpOptions);
   }
 
   getCampaignById(id: string): Observable<any> {
